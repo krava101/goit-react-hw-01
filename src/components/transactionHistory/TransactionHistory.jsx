f27b6bd68504c7a904b55bde2953346221f08589
krava <kravalex2004@gmail.com>
2024-02-12 14:06:00 +0200

    react-hw-01 1.5

--- src/components/transactionHistory/TransactionHistory.jsx
+++ src/components/transactionHistory/TransactionHistory.jsx
@@ -1,4 +1,4 @@
-import TransactionHistoryItem from "../TransactionHistoryItem/TransactionHistoryItem";
+import TransactionHistoryItem from "../TransactionHistoryItem/TransactionHistoryItem.jsx";
 import css from './TransactionHistory.module.css';
 
 const TransactionHistory = ({ items }) => 
