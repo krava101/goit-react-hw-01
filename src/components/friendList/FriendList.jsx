04c6e554bcdb349c1e1852598a8d1a3ec4905109
krava <kravalex2004@gmail.com>
2024-02-12 13:34:10 +0200

    react-hw-01

---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,15 @@
+import FriendListItem from '../friendListItem/FriendListItem';
+import css from './FriendList.module.css';
+
+const FriendList = ({ friends }) =>
+  (
+    <section>
+      <ul className={css.friendList}>
+        {friends.map(friend =>
+          (<li key={friend.id}><FriendListItem friend={friend} /></li>)
+        )}
+      </ul>
+    </section>
+  );
+
+export default FriendList;
---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,27 @@
+import css from "./Profile.module.css";
+import Stats from '../profileStats/Stats.jsx';
+
+const Profile = ({ name, tag, location, image, stats }) =>
+  (
+    <section>
+      <div className={css.card}>
+        <div className={css.cardInfo}>
+          <img
+            src={image}
+            alt="User avatar"
+          />
+          <p className={css.username}>{name}</p>
+          <p className={css.tag}>@{tag}</p>
+          <p className={css.location}>{location}</p>
+        </div>
+        <Stats
+          followers={stats.followers}
+          views={stats.views}
+          likes={stats.likes}
+        />
+      </div>
+    </section>
+  );
+
+
+export default Profile;
