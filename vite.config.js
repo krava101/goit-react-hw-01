2baeb71b4f6119b86889af0d5b81f812d9e22606
krava <kravalex2004@gmail.com>
2024-02-12 14:52:39 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  mimeTypes: {
-    'text/jsx': ['js'],
-  },
   build: {
     sourcemap: true,
   },
